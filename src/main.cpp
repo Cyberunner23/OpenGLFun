9969127f994f613c58bba93c393e952c39be7462
Cyberunner23 <narutoxela@gmail.com>
2017-08-23 20:17:25 -0400

    Simple OpenGL 3+ test with glfw.

--- src/main.cpp
+++ src/main.cpp
@@ -9,44 +9,37 @@
 #include <iostream>
 
 #include "GL/glew.h"
-#include "SDL2/SDL.h"
-#include "SDL2/SDL_opengl.h"
+#include "GLFW/glfw3.h"
 
 #include "glErrors.hpp"
 
-void SDLCrash(std::string msg) {
-    std::cout << msg << ": " << SDL_GetError() << std::endl;
-    SDL_Quit();
-    std::abort();
-}
 
-SDL_Window*   window = nullptr;
-SDL_GLContext glContext;
+GLFWwindow*   window = nullptr;
 
 int main(int argc, char **argv){
     
     //SDL Init
-    if (SDL_Init(SDL_INIT_VIDEO) < 0){
-        SDLCrash("Failed to initialize SDL");
+    if (!glfwInit()){
+        std::cout << "Failed to initialize GLFW." << std::endl;
+        return -1;
     }
     
     
     //Create Window
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,  SDL_GL_CONTEXT_PROFILE_CORE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
-    window = SDL_CreateWindow("OpenGLFun", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+    window = glfwCreateWindow(1024, 768, "OpenGLFun", NULL, NULL);
     if (window == nullptr) {
-        SDLCrash("Failed to create the window");
+        std::cout << "Failed to create a window." << std::endl;
+        glfwTerminate();
+        return -1;
     }
     
-    
-    //Init OpenGL context
-    glContext = SDL_GL_CreateContext(window);
-    if (glContext == 0) {
-        SDLCrash("Failed to create an OpenGL context");
-    }
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
     
     
     //Init GLEW
@@ -54,21 +47,19 @@ int main(int argc, char **argv){
     GLenum glewErr = glewInit();
     if (glewErr != GLEW_OK) {
         std::cout << "Failed to initialize GLEW" << std::endl;
-        SDL_Quit();
-        std::abort();
-    }
-    
-    
-    //Setup VSync
-    if (SDL_GL_SetSwapInterval(1) < 0) {
-        SDLCrash("Failed to setup VSync");
+        glfwTerminate();
+        return -1;
     }
     
     
     //OpenGL Init
     
-    const GLchar* vShaderSrc[] = { "#version 140\nin vec2 vertexPos; void main() { gl_Position = vec4( vertexPos.x, vertexPos.y, 0, 1 ); }" };
-    const GLchar* fShaderSrc[] = { "#version 140\nout vec4 frag; void main() { frag = vec4( 1.0, 1.0, 1.0, 1.0 ); }" };
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    
+    const GLchar* vShaderSrc[] = { "#version 330 core\nlayout(location = 0) in vec3 vertexPos;\nvoid main() { gl_Position.xyz = vertexPos; gl_Position.w = 1.0; }" };
+    const GLchar* fShaderSrc[] = { "#version 330 core\nout vec3 frag; void main() { frag = vec3( 1, 0, 0 ); }" };
     
     GLuint programID       = glCreateProgram();
     GLuint vShader         = glCreateShader(GL_VERTEX_SHADER);
@@ -90,8 +81,8 @@ int main(int argc, char **argv){
     if (vShaderCompiled != GL_TRUE || fShaderCompiled != GL_TRUE) {
         //I'll check what the error is when something messes up.
         std::cout << "Failed to compile a shader" << std::endl;
-        SDL_Quit();
-        std::abort();
+        glfwTerminate();
+        return -1;
     }
     
     glLinkProgram(programID);
@@ -100,76 +91,51 @@ int main(int argc, char **argv){
     glGetProgramiv(programID, GL_LINK_STATUS, &linkSuccess);
     if (linkSuccess != GL_TRUE) {
         std::cout << "Failed to link shader program" << std::endl;
-        SDL_Quit();
-        std::abort();
-    }
-    
-    GLint vertexPosLoc = glGetAttribLocation(programID, "vertexPos");
-    if (vertexPosLoc == -1) {
-        std::cout << "vertexPos is an invalid variable (in the shader)" << std::endl;
-        SDL_Quit();
-        std::abort();
+        glfwTerminate();
+        return -1;
     }
     
     glClearColor(0.f, 0.f, 0.f, 1.f);
     
     GLfloat vertices[] = {
-            -0.5f, -0.5f,
-             0.5f, -0.5f,
-             0.5f,  0.5f,
-            -0.5f,  0.5f
+            -1.0f, -1.0f, 0.0f,
+             1.0f, -1.0f, 0.0f,
+             0.0f,  1.0f, 0.0f
     };
     
-    GLuint indexData[] = {0, 1, 2, 3};
-    
     GLuint VBO;
-    GLuint IBO;
     
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, 2 * 4 * sizeof(GLfloat), vertices,  GL_STATIC_DRAW);
-    
-    glGenBuffers(1, &IBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 4 * sizeof(GLuint), indexData, GL_STATIC_DRAW);
-    
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,  GL_STATIC_DRAW);
     
     //Main loop
-    bool      quit = false;
-    SDL_Event event;
-    
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
     do {
-        
-        //Process events
-        while (SDL_PollEvent(&event)) {
-            if ((quit = event.type == SDL_QUIT || event.type == SDL_KEYDOWN)) break;
-        }
     
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
         glUseProgram(programID);
-        glEnableVertexAttribArray(vertexPosLoc);
+        glEnableVertexAttribArray(0);
         
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glVertexAttribPointer(vertexPosLoc, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), NULL);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
-        glDrawElements(GL_TRIANGLE_FAN, 4, GL_UNSIGNED_INT, NULL);
-        
-        glDisableVertexAttribArray(vertexPosLoc);
+        glDisableVertexAttribArray(0);
         glUseProgram(NULL);
     
-        SDL_GL_SwapWindow(window);
-        SDL_Delay(16);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
         
-    } while (!quit);
+    } while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS);
     
     
     //Deinit
     glDeleteProgram(programID);
-    SDL_DestroyWindow(window);
+    glfwDestroyWindow(window);
     window = nullptr;
-    SDL_Quit();
+    glfwTerminate();
     
     return 0;
 }
